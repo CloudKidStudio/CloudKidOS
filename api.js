e95f0f24bbf2736141c8225cccd729ad8ab7a48d
Matt Karl <matt@mattkarl.com>
2014-03-19 15:57:07 -0400

    Auto updated docs

---
+++ api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "bind",
+        "cloudkid.Application",
+        "cloudkid.Button",
+        "cloudkid.CacheManager",
+        "cloudkid.DragManager",
+        "cloudkid.LoaderQueueItem",
+        "cloudkid.MediaLoader",
+        "cloudkid.MediaLoaderResult",
+        "cloudkid.OS",
+        "cloudkid.PixiAnimator",
+        "cloudkid.PixiAnimator.AnimTimeline"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
