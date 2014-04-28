bb6ba6d57d222ba406f5d514bbd67e6332a9fe4e
Andrew Start <andrewstart@cloudkid.com>
2014-04-28 18:28:18 -0400

    Documented CKWorker and CombinedCallback.

--- src/cloudkid/CKWorker.js
+++ src/cloudkid/CKWorker.js
@@ -14,13 +14,18 @@
 		"this.postMessage(returnVal);" +
 	"};"*/
 	
+	//combine prefixed URL for createObjectURL from blobs.
 	window.URL = window.URL || window.webkitURL;
+	//combine prefixed blob builder
 	window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
 
 	//assign the function to the namespace
-	/** Creates a Worker or a fallback with the same API.
+	/**
+	*	Creates a Worker or a fallback with the same API.
+	*	@function createWorker
 	*	@param codeString The code in string form to make the worker from. As a string, fallback support is easier.
-	*	@return Either a Worker or a fallback with the same API to use. */
+	*	@return Either a Worker or a fallback with the same API to use.
+	*/
 	namespace("cloudkid").createWorker = function(codeString)
 	{
 		if(!window.URL || !window.Worker) return new FallbackWorker(codeString);
@@ -42,7 +47,7 @@
 			}
 			catch(error)
 			{
-				//no way of generating a blob to create worker from
+				//no way of generating a blob to create the worker from
 				return new FallbackWorker(codeString);
 			}
 		}
@@ -60,7 +65,13 @@
 		}
 	};
 	
-	/* Internal class pretends to be a Worker's context */
+	/**
+	*	Internal class that pretends to be a Web Worker's context.
+	*	@class SubWorker
+	*	@constructor
+	*	@param {String} codeString A string to evaluate into worker code.
+	*	@param {FallbackWorker} parent The FallbackWorker that owns this SubWorker.
+	*/
 	var SubWorker = function(codeString, parent)
 	{
 		this._wParent = parent;
@@ -68,15 +79,36 @@
 	};
 
 	var p = SubWorker.prototype;
-	p.onmessage = null;
 
+	/**
+	*	see https://developer.mozilla.org/en-US/docs/Web/API/Worker.onmessage
+	*	@property {Function} onmessage
+	*/
+	p.onmessage = null;
+	/**
+	*	The FallbackWorker that is controlls by this SubWorker.
+	*	@property {FallbackWorker} _wParent
+	*	@private
+	*/
+	p._wParent = null;
+
+	/**
+	*	See https://developer.mozilla.org/en-US/docs/Web/API/Worker.postMessage
+	*	@function postMessage
+	*	@param {*} data The data to send.
+	*/
 	p.postMessage = function(data)
 	{
 		var parent = this._wParent;
 		setTimeout(parent.onmessage.bind(parent, {data:data}), 1);
 	};
 	
-	/* Internal class duplicates Worker API */
+	/**
+	*	An internal class that duplicates the Worker API as a fallback when WebWorkers are not supported.
+	*	@class FallbackWorker
+	*	@constructor
+	*	@param {String} codeString A string to evaluate into worker code.
+	*/
 	var FallbackWorker = function(codeString)
 	{
 		this._wChild = new SubWorker(codeString, this);
@@ -84,12 +116,21 @@
 
 	p = FallbackWorker.prototype;
 
+	/**
+	*	See https://developer.mozilla.org/en-US/docs/Web/API/Worker.postMessage
+	*	@function postMessage
+	*	@param {*} data The data to send.
+	*/
 	p.postMessage = function(data)
 	{
 		var child = this._wChild;
 		setTimeout(child.onmessage.bind(child, {data:data}), 1);
 	};
 
+	/**
+	*	see https://developer.mozilla.org/en-US/docs/Web/API/Worker.terminate
+	*	@function terminate
+	*/
 	p.terminate = function()
 	{
 		this.onmessage = null;
@@ -99,7 +140,16 @@
 		this._wChild = null;
 	};
 
+	/**
+	*	see https://developer.mozilla.org/en-US/docs/Web/API/Worker.onmessage
+	*	@property {Function} onmessage
+	*/
 	p.onmessage = null;
+	/**
+	*	The SubWorker that is controlled by this FallbackWorker.
+	*	@property {SubWorker} _wChild
+	*	@private
+	*/
 	p._wChild = null;
 	
 }());
