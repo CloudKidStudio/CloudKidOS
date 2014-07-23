fbeec5ce12a01593d38253cfd480ce8906517ac9
Andrew Start <andrewstart@cloudkid.com>
2014-07-23 19:18:17 -0400

    Auto updated docs

---
+++ api.js
@@ -0,0 +1,38 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Application",
+        "AssetManager (PIXI)",
+        "BitmapUtils (CreateJS)",
+        "Button (CreateJS)",
+        "Button (PIXI)",
+        "CacheManager",
+        "CombinedCallback",
+        "DragManager (CreateJS)",
+        "DragManager (PIXI)",
+        "FallbackWorker",
+        "FunctionUtils (CreateJS)",
+        "LoaderQueueItem",
+        "MediaLoader",
+        "MediaLoaderResult",
+        "OS",
+        "Positioner",
+        "SavedData",
+        "ScreenSettings",
+        "SubWorker",
+        "UIElement",
+        "UIElementSettings",
+        "UIScaler",
+        "Worker"
+    ],
+    "modules": [
+        "cloudkid"
+    ],
+    "allModules": [
+        {
+            "displayName": "cloudkid",
+            "name": "cloudkid"
+        }
+    ]
+} };
+});
